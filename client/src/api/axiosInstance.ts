55e663cf040150535301181c1fca8144841adef7
sourav <130273847+sourav6251@users.noreply.github.com>
2025-04-26 16:35:47 +0530

    Update axiosInstance.ts

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:5000/api/v1",
-    baseURL:"https://college-department-management-systems.vercel.app",
+    baseURL:"https://college-department-management-systems.vercel.app/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
