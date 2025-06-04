98018bebbde4f74f794afd4148fb54391fec8e06
sourav6251 <dassourav3738+github@gmail.com>
2025-06-04 17:21:00 +0530

    latest

--- server/src/config/cors.config.js
+++ server/src/config/cors.config.js
@@ -2,7 +2,7 @@ import cors from "cors";
 
 const corsConfig = cors({
     // origin: ["*" ],
-    origin: ["http://localhost:3000","https://college-department-management-syste.vercel.app/","college-department-management-git-53c152-skds-projects-c76a720c.vercel.app" ],
+    origin: ["http://localhost:3000","https://college-department-management-syste.vercel.app","college-department-management-git-53c152-skds-projects-c76a720c.vercel.app" ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
 });
